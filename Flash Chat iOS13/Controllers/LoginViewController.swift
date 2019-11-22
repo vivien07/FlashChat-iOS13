f5184a8762c42194f8a892952edd56a6c123985e
vivien ruas <vivienruas07@gmail.com>
2019-11-22 12:24:01 +0100

    Firebase added Users can register and login

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -1,20 +1,29 @@
-//
-//  LoginViewController.swift
-//  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
-
 import UIKit
+import Firebase
+
 
 class LoginViewController: UIViewController {
 
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        
+        guard let email = emailTextfield.text, let password = passwordTextfield.text else { return }
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+          if error != nil {
+              print(error!.localizedDescription)
+            let alertVC = UIAlertController(title: "Can't login", message: error!.localizedDescription, preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self?.present(alertVC, animated: true)
+          } else {
+              self?.performSegue(withIdentifier: "LoginToChat", sender: sender)
+          }
+          
+        }
+       
     }
     
 }
--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -1,19 +1,27 @@
-//
-//  RegisterViewController.swift
-//  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
-
 import UIKit
+import Firebase
+
 
 class RegisterViewController: UIViewController {
 
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+   
+        guard let email = emailTextfield.text, let password = passwordTextfield.text else { return }
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if error != nil {
+                //print(error!.localizedDescription)
+                let alertVC = UIAlertController(title: "Can't register", message: error!.localizedDescription, preferredStyle: .alert)
+                alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alertVC, animated: true)
+            } else {
+                self.performSegue(withIdentifier: "RegisterToChat", sender: sender)
+            }
+        }
+        
     }
     
 }
