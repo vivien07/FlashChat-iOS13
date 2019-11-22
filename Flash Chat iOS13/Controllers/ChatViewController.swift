3a7fb4b86709ea26d98e10d8f0264636fe674a13
vivien ruas <vivienruas07@gmail.com>
2019-11-22 16:52:41 +0100

    tableView configured on ChatVC

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+import Firebase
 
 
 class ChatViewController: UIViewController {
@@ -9,10 +9,34 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "test@user.com", text: "Hello!"),
+        Message(sender: "a@b.fr", text:"Hi")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "⚡️FlashChat"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(logout))
+        navigationItem.hidesBackButton = true
+        tableView.dataSource = self
+        tableView.delegate = self
 
+    }
+    
+    
+    @objc func logout() {
+        
+        print("logout tapped")
+        do {
+          try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)//Pops all the view controllers on the stack except the root view controller and updates the display
+        } catch let signOutError as NSError {
+          print ("Error signing out: %@", signOutError)
+        }
+          
+        
     }
     
     
@@ -22,3 +46,37 @@ class ChatViewController: UIViewController {
     
 
 }
+
+
+
+extension ChatViewController: UITableViewDataSource {
+    
+     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath)
+        let message = messages[indexPath.row]
+        cell.textLabel?.text = message.sender
+        cell.detailTextLabel?.text = message.text
+        return cell
+       
+    }
+    
+
+    
+}
+
+
+
+extension ChatViewController: UITableViewDelegate {
+    
+    
+    
+}
+
+
